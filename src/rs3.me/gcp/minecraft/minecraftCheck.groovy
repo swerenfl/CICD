3ea3bf8bd736d5c7fc244814058505308388e26a
Richard Staehler III <richard.staehler@zoro.com>
2020-05-05 11:42:39 -0500

    adding discord to check

--- src/rs3.me/gcp/minecraft/minecraftCheck.groovy
+++ src/rs3.me/gcp/minecraft/minecraftCheck.groovy
@@ -22,16 +22,20 @@ node {
         try {
             isOffline = mc_helpers.checkUp("${G_ZONE}")
             echo "The current status of the server is: ${isOffline}"
-            if (isOffline == 'RUNNING' || isOffline == 'REPAIRING') {
-                slackColor = '#7ed321' // Green
+            if (isOffline == 'RUNNING' || isOffline == 'REPAIRING') { // Green
+                slackColor = '#7ed321'
+                discordColor = "SUCCESS"
             }
-            else if (isOffline == 'PROVISIONING' || isOffline == 'STAGING') {
-                slackColor = '#ffd806' // Yellow
+            else if (isOffline == 'PROVISIONING' || isOffline == 'STAGING') { // Yellow
+                slackColor = '#ffd806'
+                discordColor = "UNSTABLE"
             }
-            else {
-                slackColor = '#ff3366' // Red
+            else { // Red
+                slackColor = '#ff3366'
+                discordColor = "FAILURE"
             }
             slackSend channel: "${SLACK_NOTIFY_CHANNEL}", color: "${slackColor}", message: "The status of the server is ${isOffline}"
+            discordSend description: "The status of the server is ${isOffline}", footer: '', image: '', link: "${env.BUILD_URL}", result: "${discordColor}", thumbnail: '', title: "${env.JOB_BASE_NAME}", webhookURL: "${DISCORD_WEBHOOK}"
         }
         catch (err) {
             def failureMessage = 'While executing the online check something went wrong. Review logs for further details'
