359cb6bd0a9a88537ef7141a091e02abaf555197
Richard Staehler III <richard.staehler@zoro.com>
2020-05-04 23:03:37 -0500

    cleaned up StopMC

--- src/rs3.me/gcp/minecraft/minecraftStop.groovy
+++ src/rs3.me/gcp/minecraft/minecraftStop.groovy
@@ -12,35 +12,31 @@
 // Start Pipeline
 node {
 
-    // Set Build Variables
-    def gProject = 'mc-server'
-    def gZone = 'us-central1-f'
-    def emailRecp = 'richard.staehler@gmail.com'
-    def slackNotifyChannel = '#08-gaming'
+    // Load Env Variables
+    mc_variables.envVariables()
 
     // Preflight Stage
     stage ('Preflight') {
-        common_stages.startSlack("${slackNotifyChannel}")
-        common_stages.preflight("${slackNotifyChannel}")
+        common_stages.startSlack("${SLACK_NOTIFY_CHANNEL}")
+        common_stages.preflight("${SLACK_NOTIFY_CHANNEL}")
     }
 
     // Stop Minecraft Stage
     stage ('Stop Minecraft') {
         try {
-            def isOffline = mc_helpers.checkUp("${gZone}")
-            
+            def isOffline = mc_helpers.checkUp("${G_ZONE}")
             if (isOffline == "TERMINATED") {
                 echo "Nothing to do here."
             }
             else {
-                mc_helpers.stopMinecraft("${gProject}", "${gZone}")
+                mc_helpers.stopMinecraft("${G_PROJECT}", "${G_ZONE}")
             }
         }
         catch (err) {
             def failureMessage = 'While stopping the server something went wrong. Review logs for further details'
             echo "${failureMessage}" + ": " + err
             currentBuild.result = 'FAILURE'
-            common_helpers.notifySlackFail("${slackNotifyChannel}", "${failureMessage}", err)
+            common_helpers.notifySlackFail("${SLACK_NOTIFY_CHANNEL}", "${failureMessage}", err)
             throw err
         }
     }
@@ -48,8 +44,7 @@ node {
     // Verify Termination
     stage ('Verify') {
         try {
-            def isOffline = mc_helpers.checkUp("${gZone}")
-            
+            def isOffline = mc_helpers.checkUp("${G_ZONE}")
             if (isOffline == "TERMINATED") {
                 echo "Your server is indeed terminated."
             }
@@ -61,15 +56,14 @@ node {
             def failureMessage = "${e}"
             echo "${failureMessage}" + ": " + Exception
             currentBuild.result = 'FAILURE'
-            common_helpers.notifySlackFail("${slackNotifyChannel}", "${failureMessage}", err)
+            common_helpers.notifySlackFail("${SLACK_NOTIFY_CHANNEL}", "${failureMessage}", err)
             throw err
         }
     }
 
     // Notify users of the build using the emailext plugin.
     stage ('Notify') {
-        common_stages.notifyEmail("${emailRecp}", "${slackNotifyChannel}")
-        common_stages.notifySlack("${slackNotifyChannel}")
+        common_stages.notifyEmail("${EMAIL_RECP}", "${SLACK_NOTIFY_CHANNEL}")
+        common_stages.notifySlack("${SLACK_NOTIFY_CHANNEL}")
     }
-
 }
