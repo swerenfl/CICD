f05f975fea3e8389502980337ba8e8b30245a6a5
Richard Staehler III <richard.staehler@zoro.com>
2020-05-04 22:42:24 -0500

    adding env variables

--- src/rs3.me/gcp/minecraft/minecraftStart.groovy
+++ src/rs3.me/gcp/minecraft/minecraftStart.groovy
@@ -12,37 +12,25 @@
 // Start Pipeline
 node {
 
-    // Set Build Variables
-    def gProject = "mc-server"
-    def gInstance = "minecraft-project-2019-11-03"
-    def gZone = "us-central1-f"
-    def gServiceAcct = "jenkins"
-    def emailRecp = "${EMAIL_RECP}" // masking since GitHub is public.
-    def slackNotifyChannel = "#08-gaming"
-    def discordWebURL = "https://discordapp.com/api/webhooks/707044633816989787/NqD89TdUZmJBSwcKv1PyYMrEiv1uzlglPMxz2tcd43HPZ2PhB595HzGG-Hw6S0dNxbJ2"
-
-    // More Variables
+    // Load Env Variables
     mc_variables.envVariables()
 
     // Preflight Stage
     stage ('Preflight') {
-        echo sh(script: 'env|sort', returnStdout: true)
-        sh 'sleep 120'
-
-        common_stages.startSlack("${slackNotifyChannel}")
+        common_stages.startSlack("${SLACK_NOTIFY_CHANNEL}")
         //common_stages.startDiscord("${discordWebURL}")
-        common_stages.preflight("${slackNotifyChannel}")
+        common_stages.preflight("${SLACK_NOTIFY_CHANNEL}")
     }
 
     // Start Minecraft Stage
     stage ('Start Minecraft') {
-        common_stages.startMCS("${gInstance}", "${gZone}", "${gServiceAcct}", "${gProject}", "${slackNotifyChannel}")
+        common_stages.startMCS("${G_INSTANCE}", "${G_ZONE}", "${G_SERV_ACCT}", "${G_PROJECT}", "${SLACK_NOTIFY_CHANNEL}")
     }
 
     // Notify users of the build using the emailext plugin.
     stage ('Notify') {
-        common_stages.notifyEmail("${emailRecp}", "${slackNotifyChannel}")
-        common_stages.notifySlack("${slackNotifyChannel}")
+        common_stages.notifyEmail("${EMAIL_RECP}", "${SLACK_NOTIFY_CHANNEL}")
+        common_stages.notifySlack("${SLACK_NOTIFY_CHANNEL}")
         //common_stages.notifyDiscord("${discordWebURL}")
     }
 }
--- vars/mc_variables.groovy
+++ vars/mc_variables.groovy
@@ -3,9 +3,11 @@
 /* -------------------------------------------------------
                     GLOBAL VARS
 ------------------------------------------------------- */
-// add something here someday!
-
 def envVariables() {
-    env.testing = "Hello World"
-    env.foo = "bar"
+    env.GENERAL_MESSAGE = "Job Name: $JOB_NAME | Build Number: $BUILD_NUMBER | URL: $BUILD_URL"
+    env.G_PROJECT = "mc-server"
+    env.G_INSTANCE = "minecraft-project-2019-11-03"
+    env.G_ZONE = "us-central1-f"
+    env.G_SERV_ACCT = "jenkins"
+    env.SLACK_NOTIFY_CHANNEL = "#08-gaming"
 }
