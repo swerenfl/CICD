6610d8775cd4b88816199af743dc4f9ea17a154c
Richard Staehler III <richard.staehler@zoro.com>
2020-09-11 01:04:37 -0500

    lh edits

--- vars/www_helpers.groovy
+++ vars/www_helpers.groovy
@@ -4,10 +4,11 @@
 def wwwDeploy(gcKey, gBucketURL) {
     sh """
         gcloud auth activate-service-account --key-file=${gcKey}
-        gsutil -m rm ${gBucketURL}/**
         gsutil -m rsync -x '.git' -r ${WORKSPACE} ${gBucketURL}
         gsutil acl ch -u AllUsers:R ${gBucketURL}
+        gsutil setmeta -h "Content-Type:text/html" \
+            -h "Cache-Control:private, max-age=0, no-transform" ${gBucketURL}/*.html
     """
 }
-
+//        gsutil -m rm ${gBucketURL}/**
 return this
