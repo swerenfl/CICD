088b21500c26d6ce41b68150f03754c5ed0cf0cb
Richard Staehler III <richard.staehler@zoro.com>
2020-09-11 00:59:57 -0500

    lh edits

--- vars/www_helpers.groovy
+++ vars/www_helpers.groovy
@@ -6,7 +6,7 @@ def wwwDeploy(gcKey, gBucketURL) {
         gcloud auth activate-service-account --key-file=${gcKey}
         gsutil -m rm ${gBucketURL}/**
         gsutil -m rsync -x '.git' -r ${WORKSPACE} ${gBucketURL}
-        gsutil iam ch allUsers:objectViewer ${gBucketURL}
+        gsutil acl ch -u AllUsers:R ${gBucketURL}
     """
 }
 
