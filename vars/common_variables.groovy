14bde576aaa33a61babef5f213104d4846bfc602
Richard Staehler III <richard.staehler@zoro.com>
2020-11-19 22:04:54 -0600

    update creds

--- vars/common_variables.groovy
+++ vars/common_variables.groovy
@@ -8,7 +8,7 @@ def envVariables() {
     env.MC_MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
     env.GENERAL_MESSAGE = "Job Name: $JOB_NAME | Build Number: $BUILD_NUMBER | URL: $BUILD_URL"
     env.SLACK_NOTIFY_CHANNEL = "#08-gaming"
-    env.G_KEY = "48f02094-ee5b-4e1d-a9e1-99566a54da84"
+    env.G_KEY = "cd38590b-317c-4eb7-a58f-39a52d9234e4"
     env.G_PROJECT = "mc-server"
     env.G_INSTANCE = "minecraft-project-2019-11-03"
     env.G_ZONE = "us-central1-f"
