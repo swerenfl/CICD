22ed35828ca5a5281d07379667d2441817d6e316
Richard Staehler III <richard.staehler@zoro.com>
2021-10-29 01:35:42 -0500

    added TODOs and finalize

--- src/rs3.me/other/scripts/findStock.groovy
+++ src/rs3.me/other/scripts/findStock.groovy
@@ -2,7 +2,13 @@
 
 /* =============================================== */
 /*                    PIPELINE                     */
-/* =============================================== */ 
+/* =============================================== */
+
+// TODO
+// Can I check multiple items at once? Would require a FOR loop / array
+// Do I want to continue to use "common_helpers". Could I pass a value into 
+// common_stages.notifySlack to denote if success or unstable and have logic
+// to call the neccesary helper. Just a thought.
 
 // Load Library
 @Library('CICD')_
@@ -24,6 +30,8 @@ node {
     // Start Stock Stage
     stage ('Check Stock') {
         def stockResults = sh(returnStdout: true, script: """curl -Is -A "Datadog/Synthetics" "${itemCheck}" | head -1 | cut -c 8-""").trim()
+        echo "The return code for this item is ${stockResults}."
+        
         if (stockResults == "404") {
             echo "Item is out of stock."
         }
