95c56575658c341619dddd5a51764039fd0e7f34
Richard Staehler III <swerenfl@yahoo.com>
2026-01-19 14:48:06 -0600

    fix quotes]

--- vars/mc_helpers.groovy
+++ vars/mc_helpers.groovy
@@ -118,10 +118,10 @@ def sendMessage(gZone, gProject, gInstance, gServiceAcct) {
         message = "ATTENTION: Server will shutdown within the next minute for maintenance. See you soon!"
     }
     withEnv(["MC_MESSAGE=${message}"]) {
-        sh '''
+        sh """
             gcloud compute ssh --project ${gInstance} --zone ${gZone} ${gServiceAcct}@${gProject} \
-            --command="if sudo screen -list | grep -q 'fabric'; then SCREEN=fabric; else SCREEN=mcs; fi; sudo screen -S \$SCREEN -p 0 -X stuff \\\"say $MC_MESSAGE\\015\\\"; sleep 10"
-        '''
+            --command="if sudo screen -list | grep -q 'fabric'; then SCREEN=fabric; else SCREEN=mcs; fi; sudo screen -S \\$SCREEN -p 0 -X stuff \\\"say \\$MC_MESSAGE\\015\\\"; sleep 10"
+        """
     }
 }
 
